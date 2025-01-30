398d32d20a3277eceae2801db84f7455901d32c7
Ednakrz <ednakrz@lcg.unam.mx>
2025-01-30 09:48:35 -0600

    Descargué mi proyecto de interés SRP009615

--- 03.notas_RECOUN3.R
+++ 03.notas_RECOUN3.R
@@ -4,3 +4,14 @@ library("recount3")
 
 ## Revisemos todos los proyectos con datos de humano en recount3
 human_projects <- available_projects()
+
+## Encuentra tu proyecto de interés. Aquí usaremos
+## SRP009615 de ejemplo
+proj_info <- subset(
+  human_projects,
+  project == "SRP009615" & project_type == "data_sources"
+)
+
+## Crea un objeto de tipo RangedSummarizedExperiment (RSE)
+## con la información a nivel de genes
+rse_gene_SRP009615 <- create_rse(proj_info)
