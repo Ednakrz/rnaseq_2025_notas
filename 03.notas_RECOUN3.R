eb6a28c5b34dabc8edca005d6aab8313cdabb492
Ednakrz <ednakrz@lcg.unam.mx>
2025-01-30 10:51:32 -0600

    Cree la gráfica con iSEE del gen ENSG00000168314.17

--- 03.notas_RECOUN3.R
+++ 03.notas_RECOUN3.R
@@ -33,3 +33,21 @@ proj_info_interactive <- interactiveDisplayBase::display(human_projects)
 stopifnot(nrow(proj_info_interactive) == 1)
 ## Crea el objeto RSE
 rse_gene_interactive <- create_rse(proj_info_interactive)
+
+
+## Convirtamos las cuentas por nucleotido a cuentas por lectura
+## usando compute_read_counts().
+## Para otras transformaciones como RPKM y TPM, revisa transform_counts().
+assay(rse_gene_SRP009615, "counts") <- compute_read_counts(rse_gene_SRP009615)
+
+
+## Para este estudio en específico, hagamos más fácil de usar la
+## información del experimento
+rse_gene_SRP009615 <- expand_sra_attributes(rse_gene_SRP009615)
+
+colData(rse_gene_SRP009615)[
+  ,
+  grepl("^sra_attribute", colnames(colData(rse_gene_SRP009615)))
+]
+
+iSEE::iSEE(rse_gene_SRP009615)
