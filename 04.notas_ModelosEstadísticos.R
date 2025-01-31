f11945f749adbfaf8e819fbaa7e59a093939290d
Ednakrz <ednakrz@lcg.unam.mx>
2025-01-31 11:57:47 -0600

    Liempieza de los datos

--- 04.notas_ModelosEstadísticos.R
+++ 04.notas_ModelosEstadísticos.R
@@ -61,6 +61,101 @@ cowplot::plot_grid(plotlist = vd$plotlist, ncol = 1)
 
 ## --------------- Example 3 --------------------------------
 
+(sampleData = data.frame(
+  condition = factor(rep(c("ctrl_minus", "ctrl_plus",
+                           "ko_minus", "ko_plus"), 3)),
+  batch = factor(rep(1:6, each = 2))))
 
+vd <- ExploreModelMatrix::VisualizeDesign(sampleData = sampleData,
+                      designFormula = ~ 0 + batch + condition,
+                      textSizeFitted = 4, lineWidthFitted = 20,
+                      dropCols = "conditionko_minus")
+cowplot::plot_grid(plotlist = vd$plotlist, ncol = 1)
+
+app <- ExploreModelMatrix::ExploreModelMatrix(sampleData = sampleData,
+                          designFormula = ~ batch + condition)
+#> The `name` provided ('') does not correspond to a known icon
+#> The `name` provided ('hand-o-right') does not correspond to a known icon
+#> The `name` provided ('question-circle fa-1g') does not correspond to a known icon
+if (interactive()) {
+  shiny::runApp(app)
+}
+
+
+
+library("recount3")
+
+human_projects <- available_projects()
+
+rse_gene_SRP045638 <- create_rse(
+  subset(
+    human_projects,
+    project == "SRP045638" & project_type == "data_sources"
+  )
+)
+
+assay(rse_gene_SRP045638, "counts") <- compute_read_counts(rse_gene_SRP045638)
+
+rse_gene_SRP045638$sra.sample_attributes[1:3]
+
+rse_gene_SRP045638$sra.sample_attributes <- gsub("dev_stage;;Fetal\\|", "", rse_gene_SRP045638$sra.sample_attributes)
+rse_gene_SRP045638$sra.sample_attributes[1:3]
+
+rse_gene_SRP045638 <- expand_sra_attributes(rse_gene_SRP045638)
+
+colData(rse_gene_SRP045638)[
+  ,
+  grepl("^sra_attribute", colnames(colData(rse_gene_SRP045638)))
+]
+
+## Pasar de character a numeric o factor
+rse_gene_SRP045638$sra_attribute.age <- as.numeric(rse_gene_SRP045638$sra_attribute.age)
+rse_gene_SRP045638$sra_attribute.disease <- factor(tolower(rse_gene_SRP045638$sra_attribute.disease))
+rse_gene_SRP045638$sra_attribute.RIN <- as.numeric(rse_gene_SRP045638$sra_attribute.RIN)
+rse_gene_SRP045638$sra_attribute.sex <- factor(rse_gene_SRP045638$sra_attribute.sex)
+
+## Resumen de las variables de interés
+summary(as.data.frame(colData(rse_gene_SRP045638)[
+  ,
+  grepl("^sra_attribute.[age|disease|RIN|sex]", colnames(colData(rse_gene_SRP045638)))
+]))
+
+
+## Encontraremos diferencias entre muestra prenatalas vs postnatales
+rse_gene_SRP045638$prenatal <- factor(ifelse(rse_gene_SRP045638$sra_attribute.age < 0, "prenatal", "postnatal"))
+table(rse_gene_SRP045638$prenatal)
+
+## http://rna.recount.bio/docs/quality-check-fields.html
+rse_gene_SRP045638$assigned_gene_prop <- rse_gene_SRP045638$recount_qc.gene_fc_count_all.assigned / rse_gene_SRP045638$recount_qc.gene_fc_count_all.total
+summary(rse_gene_SRP045638$assigned_gene_prop)
+
+with(colData(rse_gene_SRP045638), plot(assigned_gene_prop, sra_attribute.RIN))
+
+## Hm... veamos si hay una diferencia entre los grupos
+with(colData(rse_gene_SRP045638), tapply(assigned_gene_prop, prenatal, summary))
+
+## Guardemos nuestro objeto entero por si luego cambiamos de opinión
+rse_gene_SRP045638_unfiltered <- rse_gene_SRP045638
+
+## Eliminemos a muestras malas
+hist(rse_gene_SRP045638$assigned_gene_prop)
+
+table(rse_gene_SRP045638$assigned_gene_prop < 0.3)
+
+rse_gene_SRP045638 <- rse_gene_SRP045638[, rse_gene_SRP045638$assigned_gene_prop > 0.3]
+
+gene_means <- rowMeans(assay(rse_gene_SRP045638, "counts"))
+summary(gene_means)
+
+## Eliminamos genes
+rse_gene_SRP045638 <- rse_gene_SRP045638[gene_means > 0.1, ]
+
+## Dimensiones finales
+dim(rse_gene_SRP045638)
+
+## Porcentaje de genes que retuvimos
+round(nrow(rse_gene_SRP045638) / nrow(rse_gene_SRP045638_unfiltered) * 100, 2)
+
+## ------------Normalizar los datos -------------------------------
 
 
